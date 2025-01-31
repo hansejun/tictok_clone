53557989cafc5bcfbeffc59fcafb4e37ad52c4e7
chacha <86880916+hansejun@users.noreply.github.com>
2025-02-01 00:59:55 +0900

    fix: 회원가입 반응형

--- lib/features/authentication/sign_up_screen.dart
+++ lib/features/authentication/sign_up_screen.dart
@@ -34,81 +34,105 @@ class SignUpScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: Sizes.size40,
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Gaps.v80,
-              Text(
-                "Sign up for TikTok",
-                style: TextStyle(
-                  fontSize: Sizes.size24,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-              Gaps.v20,
-              Text(
-                "Create a profile, follow other accounts, make your own videos, and more.",
-                style: TextStyle(
-                  fontSize: Sizes.size16,
-                  color: Colors.black45,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              Gaps.v40,
-              AuthButton(
-                onTap: () => _onEmailTap(context),
-                icon: FaIcon(FontAwesomeIcons.user),
-                text: "Use email & password",
-              ),
-              Gaps.v16,
-              AuthButton(
-                onTap: () => _onAppleTap(context),
-                icon: FaIcon(FontAwesomeIcons.apple),
-                text: "Continue with Apple",
-              ),
-            ],
-          ),
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.grey.shade100,
-        elevation: 2,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            vertical: Sizes.size16,
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text(
-                'Already have an account?',
-                style: TextStyle(
-                  fontSize: Sizes.size16,
-                ),
-              ),
-              Gaps.h5,
-              GestureDetector(
-                onTap: () => _onLoginTap(context),
-                child: Text(
-                  'Log in',
+    return OrientationBuilder(builder: (context, orientation) {
+      return Scaffold(
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size40,
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Gaps.v80,
+                Text(
+                  "Sign up for TikTok",
                   style: TextStyle(
-                    fontSize: Sizes.size16,
-                    fontWeight: FontWeight.w600,
-                    color: Theme.of(context).primaryColor,
+                    fontSize: Sizes.size24,
+                    fontWeight: FontWeight.w700,
                   ),
                 ),
-              ),
-            ],
+                Gaps.v20,
+                Text(
+                  "Create a profile, follow other accounts, make your own videos, and more.",
+                  style: TextStyle(
+                    fontSize: Sizes.size16,
+                    color: Colors.black45,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Gaps.v40,
+                if (orientation == Orientation.portrait) ...[
+                  AuthButton(
+                    onTap: () => _onEmailTap(context),
+                    icon: FaIcon(FontAwesomeIcons.user),
+                    text: "Use email & password",
+                  ),
+                  Gaps.v16,
+                  AuthButton(
+                    onTap: () => _onAppleTap(context),
+                    icon: FaIcon(FontAwesomeIcons.apple),
+                    text: "Continue with Apple",
+                  ),
+                ],
+                if (orientation == Orientation.landscape)
+                  Row(
+                    children: [
+                      Expanded(
+                        child: AuthButton(
+                          onTap: () => _onEmailTap(context),
+                          icon: FaIcon(FontAwesomeIcons.user),
+                          text: "Use email & password",
+                        ),
+                      ),
+                      Gaps.h16,
+                      Expanded(
+                        child: AuthButton(
+                          onTap: () => _onAppleTap(context),
+                          icon: FaIcon(FontAwesomeIcons.apple),
+                          text: "Continue with Apple",
+                        ),
+                      ),
+                    ],
+                  ),
+              ],
+            ),
           ),
         ),
-      ),
-    );
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.grey.shade100,
+          elevation: 2,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: Sizes.size16,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  'Already have an account?',
+                  style: TextStyle(
+                    fontSize: Sizes.size16,
+                  ),
+                ),
+                Gaps.h5,
+                GestureDetector(
+                  onTap: () => _onLoginTap(context),
+                  child: Text(
+                    'Log in',
+                    style: TextStyle(
+                      fontSize: Sizes.size16,
+                      fontWeight: FontWeight.w600,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    });
   }
 }
